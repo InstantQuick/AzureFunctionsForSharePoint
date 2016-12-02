73c51f95352de6b532c47454b24727e8ad5a0561
DouglasWare <DougWare@Elumenotion.com>
2016-12-02 13:31:02 -0500

    Exit silently if the client config lacks service bus configuration.

--- IQAppCommon/MessageQueueing/EnqueueMessage.cs
+++ IQAppCommon/MessageQueueing/EnqueueMessage.cs
@@ -12,6 +12,9 @@ namespace FunctionsCore
         public static void SendQueueMessage(QueuedSharePointEvent eventData)
         {
             var clientConfig = GetConfiguration(eventData.ClientId);
+
+            if (string.IsNullOrEmpty(clientConfig.NotificationQueueName) || string.IsNullOrEmpty(clientConfig.ServiceBusConnectionString)) return;
+
             QueueDescription qd = new QueueDescription(clientConfig.NotificationQueueName)
             {
                 MaxSizeInMegabytes = 5120,
